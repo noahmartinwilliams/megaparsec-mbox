37462160e4a3faea873acfb2b35447128e6d6288
Noah Williams <noahmartinwilliams@gmail.com>
2026-02-13 13:42:43 -0500

    Add Parsing Of Second From Statement

--- src/Text/Megaparsec/MBox/From.hs
+++ src/Text/Megaparsec/MBox/From.hs
@@ -1,8 +1,11 @@
 module Text.Megaparsec.MBox.From where
 
 import Control.Monad
+import Data.Either
+import Data.Set as Set
 import Text.Megaparsec
 import Text.Megaparsec.Char
+import Text.Megaparsec.EmailAddress
 import Text.Megaparsec.MBox.DateTime
 import Text.Megaparsec.MBox.Space
 import Text.Megaparsec.MBox.Types
@@ -15,3 +18,36 @@ mboxFirstFrom = do
     chars <- lexeme (some alphaNumChar)
     dt <- hlexeme mboxFromDT
     return (nums ++ ('@' : chars), dt)
+
+mboxFromName1 :: MBoxParser String
+mboxFromName1 = some letterChar
+
+mboxFromName2 :: MBoxParser String
+mboxFromName2 = do
+    void $ single '"'
+    str <- some (noneOf "\"")
+    void $ single '"'
+    return (('"' : str) ++ "\"")
+
+mboxFromName :: MBoxParser String
+mboxFromName = (mboxFromName1 <|> mboxFromName2)
+
+mboxSecondFrom :: MBoxParser (String, EAddr)
+mboxSecondFrom = do 
+    void $ string "From"
+    void $ lexeme (single ':')
+    name <- lexeme mboxFromName
+    void $ single '<'
+    inp <- getInput
+    off <- getOffset
+    st <- getParserState
+    let (st', addr) = runParser' eaddr st
+    updateParserState (\_ -> st')
+    if isLeft addr
+    then
+        let (Left e) = addr in fancyFailure (Set.fromList [(ErrorFail ("Invalid email Address. " ++ (errorBundlePretty e)))])
+    else do
+        let (Right a) = addr
+        void $ single '>'
+        return (name, a)
+
